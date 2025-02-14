2d548e6ebfcc2782a713cab423ccde5092c3e452
Qyuzet <riqyuzet@gmail.com>
2025-02-15 02:29:41 +0700

    added crash management system

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://24cc2bce7c84b7dd067ca9b63ed81f4d@o4508586519691264.ingest.us.sentry.io/4508819202637824",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
